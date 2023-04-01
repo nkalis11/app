19bb7d46ff89646023db2327889951d7968bdeea
nkalis11 <77913654+nkalis11@users.noreply.github.com>
2023-04-01 11:57:34 -1000

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,43 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { api } from "~/utils/api";
+
+const Home = () => {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <main>
+      <h1>Hello</h1>
+      <div>
+        {session ? (
+          <>
+          <p>hi {session.user?.name}</p>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => {
+              signOut().catch(console.log);
+            }}
+            >
+              Logout
+            </button>
+          </>
+        ) : (
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => {
+              signIn('discord').catch(console.log);
+            }}
+          >
+          Login with Discord
+          </button> 
+        )}
+      </div>
+    </main>
+  );
+};
+
+export default api.withTRPC(Home);
